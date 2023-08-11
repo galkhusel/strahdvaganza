919f31ab7f0f3270fa0ef8a8ac0286eb4bd55b26
FRANCISCO del Pozo <fdelpozo@fi.uba.ar>
2023-08-11 13:07:42 -0300

    pdf files can be saved

--- backend/app.py
+++ backend/app.py
@@ -3,28 +3,26 @@ from views import views
 from db import db
 from endpoints.User import users_bp, login_manager
 from endpoints.Character import characters_bp 
-import os
+
+# Create the Flask app instance
 app = Flask(__name__)
 
 # add db
 app.secret_key = "SANCTIFIED_BLOOD"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ghouls_archives.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOAD_FOLDER'] = '/inStance/PDF/'
-app.config['ALLOWED_EXTENSIONS'] = ['pdf']
-
+app.config['UPLOAD_FOLDER'] = 'D:\\Backup\\rol\\COS SMDT\\strahd 2023\\strahdvaganza\\backend\\instance\\PDF'
+app.config["ALLOWED_EXTENSIONS"] = {'pdf'}
 # Initialize the database
-
-
 db.init_app(app)
 login_manager.init_app(app)
 login_manager.login_view = 'login'
-
 
 app.register_blueprint(views, url_prefix="/views")
 app.register_blueprint(users_bp, url_prefix="/ghouls_archives/entourage")  # Update the url_prefix
 app.register_blueprint(characters_bp, url_prefix="/ghouls_archives/spectral_manifestation")
 
+# Only create the database tables when running the app directly
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
--- backend/endpoints/Character.py
+++ backend/endpoints/Character.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, jsonify, request
 from db import Character, db 
+import os
 
-
+# relative path
+PDF_FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'instance', 'PDF'))
 
 
 # Create a blueprint for characters-related endpoints
@@ -22,13 +24,13 @@ def update_character(character_id):
     updated_data = {'id': character.ID, 'name': character.Name, 'class': character.ClassID, 'item': character.ItemID}
     return jsonify(updated_data)
 
+
+
 @characters_bp.route('/hero/<int:character_id>/sign_blood_covenant', methods=['POST'])
 def upload_pdf(character_id):
     character = Character.query.get(character_id)
-    
-    if request.files:
-        print("request.files")
-    if not 'file' in request.files or 'pdf' not in request.files:
+
+    if 'pdf' not in request.files:
         return jsonify({"error": "No PDF file provided"}), 400
     
     pdf_file = request.files['pdf']
@@ -36,11 +38,17 @@ def upload_pdf(character_id):
     if pdf_file.filename == '':
         return jsonify({"error": "No selected PDF file"}), 400
     
-    # You can save the PDF file using a unique name to avoid overwriting
+    # Construct the PDF file path using the PDF_FILE_PATH
     pdf_filename = f'character_{character_id}.pdf'
-    pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], pdf_filename)
+    pdf_path = os.path.join(PDF_FILE_PATH, pdf_filename)
+    print(pdf_file)
+    # Ensure the PDF folder exists, create if not
+    os.makedirs(os.path.dirname(pdf_path), exist_ok=True)
+    # Before saving the file
+    print("pdf_path:", pdf_path)
     pdf_file.save(pdf_path)
-    
+    # After saving the file
+    print("PDF saved at:", pdf_path)
     character.PDF = pdf_path
     db.session.commit()
     
