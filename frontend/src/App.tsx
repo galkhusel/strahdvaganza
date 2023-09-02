ed85faf9b117d0925e0367ea5f964288f2d71e07
mmedela <mmedela@fi.uba.ar>
2023-09-02 19:27:10 -0300

    create login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Login } from './pages/login/Login'
+import { Home } from './pages/home/Home'
+import { CharacterCreation } from './pages/characterCreation/CharacterCreation'
+import { Payments } from './pages/payments/Payments'
+import { Header } from './assets/common/header/Hedar'
 
 function App() {
 
   return (
-    <div className='flex items-center justify-center'>
-      <Login/>
+    <div>
+      <BrowserRouter>
+        <Header/>
+        <main>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/character-creation' element={<CharacterCreation/>}/>
+            <Route path='/payment' element={<Payments/>}/>
+            <Route path='/login' element={<Login/>}/>
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   )
 }
