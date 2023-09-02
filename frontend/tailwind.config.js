4453b0036d144a4317cf3c7df679cb869b4909af
mmedela <mmedela@fi.uba.ar>
2023-09-02 14:24:45 -0300

    define gothic color scheme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-purple": "#4a004b",
+        "blood-red": "#8b0000",
+        "elegant-black": "#0b0c10",
+        "mysterious-gray": "#2b2d42",
+        "dusty-rose": "#d90429"
+      }
+    }
   },
   plugins: [],
 }
