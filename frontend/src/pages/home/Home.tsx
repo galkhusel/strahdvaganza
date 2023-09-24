9d6a988c82dce23f619a9e3ff66422509e676d5e
FRANCISCO del Pozo <fdelpozo@fi.uba.ar>
2023-09-24 10:57:34 -0300

    added structure for rulest

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -3,6 +3,8 @@ export const Home = () => {
     const userCredentilas = JSON.parse(localStorage.getItem('userCredentials') ?? '');
 
   return (
-    <div>Welcome {userCredentilas.name}</div>
+    <>
+      <div>Welcome {userCredentilas.name}</div>
+    </>
   )
 }
